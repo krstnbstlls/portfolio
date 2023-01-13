d51b61d7c9449fc8c94f755cee7b0b7aa499d3b6
Kristina Bustillos <122511267+krstnbstlls@users.noreply.github.com>
2023-01-14 02:09:14 +0800

    Portfolio final

---
+++ javascripts/typingeffect.js
@@ -0,0 +1,10 @@
+  	$("#typed").typed({
+  		strings: ["Software Developer"],
+  		typeSpeed: 100,
+  		startDelay: 300,
+  		backSpeed: 60,
+  		backDelay: 2000,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
