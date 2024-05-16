1d4045a6ecb07b0439f9f2a4572d4ece18c8fbaf
Kristina Bustillos <122511267+krstnbstlls@users.noreply.github.com>
2024-05-16 17:39:47 +0800

    Add files via upload

---
+++ javascripts/typingeffect.js
@@ -0,0 +1,14 @@
+  	$("#typed").typed({
+		strings: [
+			"Web Developer",
+			"Front-End Developer",
+			"Software Developer",
+		],
+  		typeSpeed: 100,
+  		startDelay: 300,
+  		backSpeed: 60,
+  		backDelay: 2000,
+  		loop: true,
+  		cursorChar: "|",
+  		contentType: 'html'
+  	});
